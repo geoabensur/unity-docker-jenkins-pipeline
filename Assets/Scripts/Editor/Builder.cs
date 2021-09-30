3d2faa177dd56d296706d7ad7373bd6dba3120d6
Geovana Amorim Abensur <geovana.abensur@SMANB12203.local>
2021-09-30 04:08:54 -0400

    Add unity project

---
+++ Assets/Scripts/Editor/Builder.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using System;
+
+namespace Builder.Editor {
+
+    public static class Builder
+    {
+        public static void BuildAndroid()
+        {
+            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+            buildPlayerOptions.scenes = new[] { "Assets/Scene/SampleScene.unity", "Assets/Scene2.unity" };
+            buildPlayerOptions.target = BuildTarget.Android;
+            buildPlayerOptions.options = BuildOptions.None;
+            BuildPipeline.BuildPlayer(buildPlayerOptions);
+        }
+    }
+}
